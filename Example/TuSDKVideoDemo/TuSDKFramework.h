c70952049caf1bc06a9482729127ba0ee12f062d
lpc <liupengcheng@tusdk.com>
2022-03-16 14:54:59 +0800

    [ update ] : 静态库提交

--- Example/TuSDKVideoDemo/TuSDKFramework.h
+++ Example/TuSDKVideoDemo/TuSDKFramework.h
@@ -6,9 +6,9 @@
 //  Copyright © 2016 TuSDK. All rights reserved.
 //
 
-//#import "TuSDK.h"
-//#import "TuSDKVideo.h"
-//#import "TuSDKFace.h"
-#import <TuSDK/TuSDK.h>
-#import <TuSDKVideo/TuSDKVideo.h>
-#import <TuSDKFace/TuSDKFace.h>
+#import "TuSDK.h"
+#import "TuSDKVideo.h"
+#import "TuSDKFace.h"
+//#import <TuSDK/TuSDK.h>
+//#import <TuSDKVideo/TuSDKVideo.h>
+//#import <TuSDKFace/TuSDKFace.h>
